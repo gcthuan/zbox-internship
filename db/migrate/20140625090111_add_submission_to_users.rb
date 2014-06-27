21e84c74111c5755ba8be0d9369100c788da2784
Altair <gcthuan@apcs.vn>
2014-06-27 13:56:18 +0700

    Fix heroku

--- db/migrate/20140625090111_add_submission_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddSubmissionToUsers < ActiveRecord::Migration
-  def self.up
-  	add_attachment :users, :submission
-  end
-
-  def self.down
-  	remove_attachment :users, :submission
-  end
-end
--- db/migrate/20140626041737_add_submission_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddSubmissionToUsers < ActiveRecord::Migration
-  def self.up
-    add_column :users, :submission, :string
-  end
-  def self.down
-	remove_column :users, :submission
-  end
-end
