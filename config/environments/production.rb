c767d4c8e2c2eee2cf3c210c7ff38ba8cd889c2a
Altair <gcthuan@apcs.vn>
2014-07-07 12:25:05 +0700

    Fix heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,15 @@ ZboxInternship::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  ActionMailer::Base.smtp_settings[:enable_starttls_auto] = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    authentication: "plain",
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"],
+    enable_starttls_auto: false
+  }
+  config.action_mailer.raise_delivery_errors = true
+
 end
